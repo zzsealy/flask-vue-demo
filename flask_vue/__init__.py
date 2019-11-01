519fb9f53a10f3eb075c8b52b5eb602da0d0f85e
ruiquan.dai <1147575371@qq.com>
2019-11-01 11:42:03 +0800

    重构了一下

--- flask_vue/__init__.py
+++ flask_vue/__init__.py
@@ -1,13 +1,27 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_vue.extends import cors, sqlalchemy, migrate
 from config import Config
+from flask_vue.api import bp as api_bp
+from flask_sqlalchemy import SQLAlchemy 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(config_class)
-    CORS(app)
+    config_app(app, config_class)
+    register_extends(app)
+    register_blueprint(app)
     # 注册蓝本
-    from flask_vue.api import bp as api_bp
+    return app
+
+
+def register_extends(app):
+    cors.init_app(app)
+    sqlalchemy.init_app(app)
+    migrate.init_app(app)
+
+
+def register_blueprint(app):
     app.register_blueprint(api_bp, url_prefix = '/api')
 
-    return app
+
+def config_app(app, config_class):
+    app.config.from_object(config_class)
