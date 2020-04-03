7e5e4b3f6e5a0eb946cc6cfbfa81b70dc0f06d85
ruiquan.dai <1147575371@qq.com>
2020-04-03 11:07:51 +0800

    eslint

--- flask_vue/api/ping.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from flask_vue.api import bp
-
-@bp.route('/ping', methods=['GET'])
-def ping():
-    ''' 用来测试连通性 '''
-    return jsonify('Pong!')
-
--- wsgi.py
+++
@@ -1,3 +0,0 @@
-from flask_vue import create_app
-
-app = create_app()
